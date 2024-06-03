1d9c044fe95a12d6d412d01379e482e5583efff5
2BDevTeam <github@2business-si.com>
2024-06-03 15:45:26 +0200

    Add project files.

---
+++ FIPAG_KOBOTOOLBOX/Controllers/KOBOController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FIPAG_KOBOTOOLBOX.Domains.Interfaces;
+
+namespace FIPAG_KOBOTOOLBOX.Controllers
+{
+    public class KOBOController : ControllerBase
+    {
+        
+    }
+}
