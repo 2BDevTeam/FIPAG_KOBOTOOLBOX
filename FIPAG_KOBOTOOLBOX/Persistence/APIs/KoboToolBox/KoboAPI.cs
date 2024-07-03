554891e9db34717b7206dd874572e8e11e5b98a6
2BDevTeam <github@2business-si.com>
2024-07-03 21:02:27 +0200

    asd

--- FIPAG_KOBOTOOLBOX/Persistence/APIs/KoboToolBox/KoboAPI.cs
+++ FIPAG_KOBOTOOLBOX/Persistence/APIs/KoboToolBox/KoboAPI.cs
@@ -442,7 +442,7 @@ namespace FIPAG_KOBOTOOLBOX.Persistence.APIs.KoboToolBox
         {
 
             int start = 0;
-            int limit = 100;
+            int limit = 1500;
 
             try
             {
