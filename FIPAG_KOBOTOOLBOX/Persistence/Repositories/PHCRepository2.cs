0ac0fbbc52675c01ec988b8f16c8a74c39319766
2BDevTeam <github@2business-si.com>
2024-07-04 13:56:58 +0200

    asd

--- FIPAG_KOBOTOOLBOX/Persistence/Repositories/PHCRepository2.cs
+++ FIPAG_KOBOTOOLBOX/Persistence/Repositories/PHCRepository2.cs
@@ -118,6 +118,7 @@ namespace FIPAG_KOBOTOOLBOX.Persistence.Repositories
                         //&& sq.Ousrdata.Date != today
                         //&& sq.Ousrdata.Date != yesterday
                         )
+                .Take(1000)
                .ToList();
         }
 
