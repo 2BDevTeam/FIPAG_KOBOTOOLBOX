408b321fb7e13c60a5ea8f4514e4da1ca79a2c9a
2BDevTeam <github@2business-si.com>
2024-07-11 00:55:14 +0200

    asd

--- FIPAG_KOBOTOOLBOX/Jobs/CronJobs.cs
+++ FIPAG_KOBOTOOLBOX/Jobs/CronJobs.cs
@@ -20,12 +20,12 @@ namespace FIPAG_KOBOTOOLBOX.Jobs
             RecurringJob.AddOrUpdate(
              "ProcessarFormularios_AdRC_Nacala",
              () => KOBOService.ProcessarFormularios("OnBD_AdRN_Nacala", "Cidade_de_Nacala"),
-            Cron.Daily());
+            Cron.Hourly());
 
             RecurringJob.AddOrUpdate(
              "ProcessarFormularios_AdRC_Pemba",
              () => KOBOService.ProcessarFormularios("OnBD_AdRN_Pemba", "Cidade_de_Pemba"),
-            Cron.Daily());
+            Cron.Hourly());
 
             /*
             RecurringJob.AddOrUpdate(
--- FIPAG_KOBOTOOLBOX/Persistence/Repositories/PHCRepository2.cs
+++ FIPAG_KOBOTOOLBOX/Persistence/Repositories/PHCRepository2.cs
@@ -118,7 +118,7 @@ namespace FIPAG_KOBOTOOLBOX.Persistence.Repositories
                         //&& sq.Ousrdata.Date != today
                         //&& sq.Ousrdata.Date != yesterday
                         )
-                .Take(50)
+                .Take(250)
                .ToList();
         }
 
