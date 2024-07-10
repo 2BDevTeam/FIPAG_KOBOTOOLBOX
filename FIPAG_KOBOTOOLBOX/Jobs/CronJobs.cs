0f9148f870a394554426b38326ed51f29b11d277
2BDevTeam <github@2business-si.com>
2024-07-10 18:46:13 +0200

    AdRN - init

--- FIPAG_KOBOTOOLBOX/Jobs/CronJobs.cs
+++ FIPAG_KOBOTOOLBOX/Jobs/CronJobs.cs
@@ -17,19 +17,29 @@ namespace FIPAG_KOBOTOOLBOX.Jobs
         public void JobHandler()
         {
 
+            RecurringJob.AddOrUpdate(
+             "ProcessarFormularios_AdRC_Nacala",
+             () => KOBOService.ProcessarFormularios("OnTS_OBA_AdRN_Nacala", "Cidade_de_Nacala"),
+            Cron.Daily());
+
+            RecurringJob.AddOrUpdate(
+             "ProcessarFormularios_AdRC_Pemba",
+             () => KOBOService.ProcessarFormularios("OnTS_OBA_AdRN_Pemba", "Cidade_de_Pemba"),
+            Cron.Daily());
 
             /*
             RecurringJob.AddOrUpdate(
                "ProcessarFormularios_AdRC_MOATIZE",
                () => KOBOService.ProcessarFormularios("OnBD_AdRC_MOATIZE", "Moatize"),
-              Cron.Yearly());
+              Cron.Daily());
             */
 
-
+            /*
             RecurringJob.AddOrUpdate(
                "ProcessarFormularios_OnBD_FIPAG_Beira",
                () => KOBOService.ProcessarFormularios("OnBD_Fipag_Beira", "Beira"),
               Cron.Daily());
+            */
 
             /*
             RecurringJob.AddOrUpdate(
@@ -38,26 +48,6 @@ namespace FIPAG_KOBOTOOLBOX.Jobs
               Cron.Hourly());
              */
 
-
-            /*
-            RecurringJob.AddOrUpdate(
-               "SincronizarFt",
-               () => KOBOService.SincronizarFt(),
-              Cron.Minutely());
-            */
-
-
-
-            /*-------Jobs Temporários para sincronização de beneficiários já existentes no PHC-------*/
-            /*
-            RecurringJob.AddOrUpdate(
-               "AddClDoKoboParaPHC",
-               () => koboService.AdicionarLigacoesDeCls("OnBD"),
-              Cron.Minutely());
-
-
-            */
-            /*------- FIM -------*/
 
 
         }
